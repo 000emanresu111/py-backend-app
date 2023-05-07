bb7028b2bd9022775e7ce64265a2ac333d986bed
Andrea Evangelista <andrea.evangelista@techedgegroup.com>
2023-05-07 14:10:43 +0200

    feat(py-backend-app): add check on username when creating new user; add playground for populating tables

--- app/crud.py
+++ app/crud.py
@@ -1,4 +1,4 @@
-from typing import List, Type
+from typing import List, Type, Optional
 import bcrypt
 from sqlalchemy.orm import Session
 from fastapi import HTTPException
@@ -15,17 +15,20 @@ def get_user_by_username(db: Session, username: str):
     return db.query(models.UserInfo).filter(models.UserInfo.username == username).first()
 
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str) -> Optional[schema.User]:
     return db.query(models.UserInfo).filter(models.UserInfo.email == email).first()
 
 
-def get_user_by_id(db: Session, user_id: int):
+def get_user_by_id(db: Session, user_id: int) -> Optional[schema.User]:
     return db.query(models.UserInfo).filter(models.UserInfo.id == user_id).first()
 
 
-def create_user(db: Session, user: schema.User):
+def create_user(db: Session, user: schema.User) -> schema.User:
     if get_user_by_email(db, user.email):
         raise HTTPException(status_code=400, detail="Email already registered")
+
+    if get_user_by_username(db, user.username):
+        raise HTTPException(status_code=400, detail="Username already registered")
 
     hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
 
---
+++ playground.py
@@ -0,0 +1,53 @@
+import psycopg2
+from itertools import product
+
+db_params = {
+    "host": "127.0.0.1",
+    "port": 5432,
+    "user": "postgres",
+    "password": "password123",
+    "database": "postgres",
+}
+
+conn = psycopg2.connect(**db_params)
+conn.autocommit = True
+
+cur = conn.cursor()
+cur.execute("SELECT 1 FROM pg_database WHERE datname='backend_db'")
+if cur.fetchone():
+    cur.execute("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'backend_db';")
+    cur.execute("DROP DATABASE backend_db")
+    print("backend_db database dropped successfully")
+
+cur.execute("CREATE DATABASE backend_db")
+print("backend_db database created successfully")
+
+cur.execute("SELECT EXISTS(SELECT 1 FROM information_schema.tables WHERE table_name = 'users')")
+exists = cur.fetchone()[0]
+if not exists:
+    cur.execute("""
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            is_2fa_enabled BOOLEAN NOT NULL
+        )
+    """)
+    print("users table created successfully")
+
+emails = ["test1@example.com", "test2@example.com", "test3@example.com"]
+passwords = ["password1", "password2", "password3"]
+usernames = ["User1", "User2", "User3"]
+is_2fa_enabled_values = [True, False]
+
+values = product(emails, passwords, usernames, is_2fa_enabled_values)
+
+cur.executemany("""
+    INSERT INTO users (email, password, username, is_2fa_enabled)
+    VALUES (%s, %s, %s, %s)
+""", values)
+print("users table populated successfully")
+
+cur.close()
+conn.close()
